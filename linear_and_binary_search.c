a12f788d571b0e36e350842b50d8c3a45e9b1566
Abdolrahman Farshgar <ifarshgar@gmail.com>
2020-12-04 02:41:01 +0200

    Add files via upload

---
+++ linear_and_binary_search.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+struct StudentInfo{
+   char studentNo[5];
+   char name[10];
+   int grade;
+} s[10];
+
+char * toLower(char *str) {
+    int n = strlen(str);
+    char *lower = (char *) malloc(n+1 * sizeof(char));
+
+    for(int i=0; i<strlen(str); i++)
+        lower[i] = tolower(str[i]);
+    lower[n] = '\0';
+
+    return lower;
+}
+
+void binary_search(char *target, int size){
+   int low = 0;
+   int high = size;
+
+   int mid;
+   while (low <= high){
+       mid = (high + low)/2;
+       if(strcmp(s[mid].studentNo,target)==0){
+           printf("\nNo.  ---- Name ---- Grades\n");
+           printf("%s ---- %s ---- %d",s[mid].studentNo, s[mid].name, s[mid].grade);
+           return;
+       }
+       if(strcmp(s[mid].studentNo,target) > 0){
+           high = high - 1;
+       }
+       else{
+           low = mid + 1;
+       }
+   }
+   printf("\nStudent not found!!");
+}
+
+void linear_search(char *student_name, int size) {
+   int i;
+
+   student_name = toLower(student_name);
+
+   for(i=0;i<=size;i++){
+       char *ls = toLower(s[i].name);
+       if(strcmp(ls, student_name)==0){
+           printf("\nNo.  ---- Name ---- Grades\n");
+           printf("%s ---- %s ---- %d",s[i].studentNo, s[i].name, s[i].grade);
+           return;
+       }
+   }
+   printf("\n Student not found!!");
+
+}
+int main(int argc, char *argv[]) {
+    int cnt = 0;
+
+    FILE *stud_file = fopen("student.txt","r");
+    if(stud_file == NULL) {
+        printf("File not found!\n");
+        exit(1);
+    }
+
+    char line[100];
+    while(fgets(line, 100, stud_file) != NULL){
+        if(strcmp(line, "\n") == 0)
+            continue;
+
+        char *token = strtok(line, " ");
+
+        strcpy(s[cnt].studentNo, token);
+        s[cnt].studentNo[4] = '\0';
+
+        token = strtok(NULL, " ");
+        strcpy(s[cnt].name, token);
+
+        token = strtok(NULL, " ");
+        s[cnt].grade = atoi(token);
+
+        cnt++;
+    }
+    fclose(stud_file);
+
+    // print everything
+    for(int i=0; i<cnt; i++) {
+        printf("%s %s %d\n", s[i].studentNo, s[i].name, s[i].grade);
+    }
+
+    int choice = 1;
+    while( choice != 3){
+       printf("\n1.Find Student by number.");
+       printf("\n2.Find Student by name.");
+       printf("\n3.Exit.");
+       printf("\nEnter your choice from above: ");
+       scanf("%d", &choice);
+
+       if(choice == 1){
+           char stud_number[5];
+           printf("\nEnter student registration number: ");
+           scanf("%s", stud_number);
+           binary_search(stud_number, cnt);
+           printf("\n=========================\n");
+       }
+
+       else if(choice == 2){
+           char stud_name[10];
+           printf("\nEnter student name: ");
+           scanf("%s", stud_name);
+           linear_search(stud_name, cnt);
+           printf("\n==========================\n");
+       }
+
+       else if(choice == 3){
+           printf("\nGood-bye");
+       }
+    }
+
+    return 0;
+}
---
+++ stack.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+struct stack{
+    int top;
+    char values[MAX];
+} myStack;
+
+void push(char v)
+{
+    if(myStack.top == MAX-1)
+        printf("Stack is full");
+    else
+    {
+        myStack.top++;
+        myStack.values[myStack.top]=v;
+    }
+}
+
+char pop()
+{
+    if(myStack.top <= -1)
+    {
+        printf("\n\t Stack is empty");
+    }
+    else
+    {
+        char c = myStack.values[myStack.top];
+        myStack.top--;
+        return c;
+    }
+}
+
+int size() {
+    return myStack.top + 1;
+}
+
+void print()
+{
+    printf("Stack: ");
+    int i;
+    for(i=0;i <= myStack.top; i++)
+    {
+        printf("%c ", myStack.values[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    myStack.top = -1;
+    char str[500];
+
+    while(strcmp(str, "exit") != 0) {
+        printf("Enter string: ");
+        gets(str);
+
+        int i,j;
+        for(i=0; str[i] != '\0'; i++)
+        {
+            if(str[i] >= 'A' && str[i] <= 'Z') {
+                push(str[i]);
+                printf("size: %d\n", size());
+            }
+            else if(str[i]>= '0' && str[i]<='9')
+            {
+                int pos = str[i] - '0';
+                for(j=0;j<pos;j++)
+                {
+                    char c = pop();
+                    printf("Popped: %c",c);
+                    printf("\n");
+                    printf("size: %d\n", size());
+                }
+            }
+            else if(str[i] == '$')
+            {
+                printf("size: %d\n", size());
+                print();
+            }
+            else
+                continue;
+        }
+    }
+
+    return 0;
+}
